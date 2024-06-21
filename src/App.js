618dc2f45e61e14828b65a9d54b8ff9b93babca9
unknown <kevin018111@gmail.com>
2024-06-21 23:50:16 +0900

    feat: code update:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import {
     BrowserRouter
 } from "react-router-dom";
@@ -8,20 +6,7 @@ function App() {
     return (
         <BrowserRouter basename={"portfolio-site"}>
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+
             </div>
         </BrowserRouter>
     );
