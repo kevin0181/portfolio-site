44f006c17cec791564984f46b023e877e4ff6ce9
youngbeen you <kevin018111@gmail.com>
2024-06-26 16:47:57 +0900

    fix: github pages 배포 시, 새로고침하면 404 오류나는거 해결

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import {
-    BrowserRouter, Route, Routes
+    BrowserRouter, Route, Routes, useLocation
 } from "react-router-dom";
 import Loading from "./load/Loading";
 import Main from "./Main";
 
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+function RedirectHandler() {
+    const query = useQuery();
+    const path = query.get('');
+
+    if (path) {
+        window.history.replaceState(null, '', path);
+    }
+
+    return null;
+}
+
 function App() {
     return (
-        <BrowserRouter basename={"portfolio-site"}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Routes>
-                    <Route path={"/"} element={<Loading/>}/>
                     <Route path={"/main"} element={<Main/>}/>
+                    <Route path={"/"} exact element={<Loading/>}/>
+                    <Route component={RedirectHandler} />
                 </Routes>
             </div>
         </BrowserRouter>
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,19 @@
+import {Canvas} from "@react-three/fiber";
+import {Box, OrbitControls} from "@react-three/drei";
+import "./css/main.css";
+
 let Main = () => {
     return (
-        <>main</>
+        <div className={"container"}>
+            <Canvas>
+                <ambientLight intensity={0.5}/>
+                <directionalLight position={[2, 1, 3]} intensity={0.5}/>
+                <OrbitControls/>
+                <Box position={[0, 0, 0]} args={[3, 3, 3]}>
+                    <meshStandardMaterial color="springgreen"/>
+                </Box>
+            </Canvas>
+        </div>
     )
 }
 
