1091eff80161ba5632941cc066f42e3004e7cb16
youngbeen you <kevin018111@gmail.com>
2024-06-26 17:31:16 +0900

    update: 카메라 및 바탕 구현

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,25 @@
-import {Canvas} from "@react-three/fiber";
-import {Box, OrbitControls} from "@react-three/drei";
+import {Canvas, useFrame} from "@react-three/fiber";
+import {Box, OrbitControls, Plane} from "@react-three/drei";
 import "./css/main.css";
+import {useEffect, useRef} from "react";
 
 let Main = () => {
     return (
         <div className={"container"}>
-            <Canvas>
-                <ambientLight intensity={0.5}/>
-                <directionalLight position={[2, 1, 3]} intensity={0.5}/>
+            <Canvas shadows camera={{position: [0.5, 1, -0.1], fov: 100}}>
+                <ambientLight intensity={3}/>
+                <directionalLight position={[3, 3, -3]} intensity={0.5}
+                                  castShadow
+                                  shadow-mapSize-width={1024}
+                                  shadow-mapSize-height={1024}
+                />
                 <OrbitControls/>
-                <Box position={[0, 0, 0]} args={[3, 3, 3]}>
-                    <meshStandardMaterial color="springgreen"/>
+                <Box position={[0, 0, 0]} args={[10, 0.1, 10]} receiveShadow>
+                    <meshStandardMaterial color="#d67c38"/>
+                </Box>
+
+                <Box position={[0, 0.15, 0]} args={[1, 0.2, 0.5]} castShadow>
+                    <meshStandardMaterial color="red"/>
                 </Box>
             </Canvas>
         </div>
