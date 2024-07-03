2824f7003578c329c3829277392215e4d264169c
youngbeen you <kevin018111@gmail.com>
2024-07-03 17:24:10 +0900

    update: car -> 구체로 변경 & 방향키에 따라서 움직이게 하기 성공

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
-import {Canvas} from "@react-three/fiber";
-import {Box, OrbitControls} from "@react-three/drei";
-import {Physics, useBox, Debug} from "@react-three/cannon";
+import React, { useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Sphere, OrbitControls } from "@react-three/drei";
+import {Physics, useSphere, Debug, useBox} from "@react-three/cannon";
 import "./css/main.css";
 
 const Ground = (props) => {
@@ -12,23 +13,88 @@ const Ground = (props) => {
     }));
     return (
         <mesh ref={ref} receiveShadow>
-            <boxGeometry args={[100, 0.1, 100]}/>
-            <meshStandardMaterial color="#d67c38"/>
+            <boxGeometry args={[100, 0.1, 100]} />
+            <meshStandardMaterial color="#d67c38" />
         </mesh>
     );
 };
 
 const Car = (props) => {
-    const [ref] = useBox(() => ({
+    const [ref, api] = useSphere(() => ({
         mass: 4,
-        position: [0, 1, 0], // 자동차가 땅 위에서 시작하도록 설정
+        position: [0, 1, 0],
         ...props,
-        args: [0.3, 0.05, 0.2], // 자동차의 크기 지정
+        args: [0.1], // 공의 반지름을 지정합니다.
     }));
+
+    const [move, setMove] = useState({
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+    });
+
+    const handleKeyDown = (event) => {
+        switch (event.key) {
+            case "ArrowUp":
+                setMove((prev) => ({ ...prev, forward: true }));
+                break;
+            case "ArrowDown":
+                setMove((prev) => ({ ...prev, backward: true }));
+                break;
+            case "ArrowLeft":
+                setMove((prev) => ({ ...prev, left: true }));
+                break;
+            case "ArrowRight":
+                setMove((prev) => ({ ...prev, right: true }));
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleKeyUp = (event) => {
+        switch (event.key) {
+            case "ArrowUp":
+                setMove((prev) => ({ ...prev, forward: false }));
+                break;
+            case "ArrowDown":
+                setMove((prev) => ({ ...prev, backward: false }));
+                break;
+            case "ArrowLeft":
+                setMove((prev) => ({ ...prev, left: false }));
+                break;
+            case "ArrowRight":
+                setMove((prev) => ({ ...prev, right: false }));
+                break;
+            default:
+                break;
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
+        };
+    }, []);
+
+    useFrame(() => {
+        const velocity = [0, 0, 0];
+        if (move.forward) velocity[0] -= 5;
+        if (move.backward) velocity[0] += 5;
+        if (move.left) velocity[2] += 5;
+        if (move.right) velocity[2] -= 5;
+        api.velocity.set(velocity[0], velocity[1], velocity[2]);
+    });
+
     return (
         <mesh ref={ref} castShadow>
-            <boxGeometry args={[0.3, 0.05, 0.2]}/>
-            <meshStandardMaterial color="red"/>
+            <sphereGeometry args={[0.1]} /> {/* 공의 반지름을 지정합니다. */}
+            <meshStandardMaterial color="red" />
         </mesh>
     );
 };
@@ -36,8 +102,8 @@ const Car = (props) => {
 const Main = () => {
     return (
         <div className={"container"}>
-            <Canvas shadows camera={{position: [0.5, 1, -0.1], fov: 70}}>
-                <ambientLight intensity={3}/>
+            <Canvas shadows camera={{ position: [0.5, 1, -0.1], fov: 70 }}>
+                <ambientLight intensity={3} />
                 <directionalLight
                     position={[3, 3, -3]}
                     intensity={0.5}
@@ -45,11 +111,11 @@ const Main = () => {
                     shadow-mapSize-width={1024}
                     shadow-mapSize-height={1024}
                 />
-                <OrbitControls/>
+                <OrbitControls />
                 <Physics gravity={[0, -9.81, 0]}> {/* 중력 설정 */}
-                    <Debug/> {/* 물리 객체를 시각화하여 디버깅 */}
-                    <Ground/>
-                    <Car/>
+                    <Debug /> {/* 물리 객체를 시각화하여 디버깅 */}
+                    <Ground />
+                    <Car />
                 </Physics>
             </Canvas>
         </div>
