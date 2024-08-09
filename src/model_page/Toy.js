78f294e9dc5bc6889bb75a81413efe6fe1ed0881
youngbeen you <kevin018111@gmail.com>
2024-08-09 13:05:25 +0900

    feat: 볼링핀 자리 배치 완료

--- src/model_page/Toy.js
+++ src/model_page/Toy.js
@@ -64,8 +64,8 @@ const Pin = (props) => {
     const [bodyRef] = useCompoundBody(() => ({
         mass: 0.1,
         shapes: [
-            {type: 'Sphere', args: [0.5], position: [0, -0.1, 0], mass: 0.5}, // lower cylinder
-            {type: 'Sphere', args: [0.45], position: [0, 0.1, 0], mass: 0.5},  // upper sphere
+            {type: 'Sphere', args: [0.25], position: [0, -0.1, 0], mass: 0.5}, // lower cylinder
+            {type: 'Sphere', args: [0.25], position: [0, 0.1, 0], mass: 0.5},  // upper sphere
         ],
         position: props.position,
         ...props,
@@ -88,8 +88,12 @@ let Toy = () => {
             <Duck/>
             <Truck/>
             <Rocket/>
-            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[-2, 0.5, -3]}/>
-            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[-4, 0.5, -3]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[3.75, 0.5, 7.5]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[3.5, 0.5, 8]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[4, 0.5, 8]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[4.25, 0.5, 8.5]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[3.75, 0.5, 8.5]}/>
+            <Pin fbx={'./models/bowling_pin.fbx'} scale={0.02} position={[3.25, 0.5, 8.5]}/>
         </>
     );
 }
