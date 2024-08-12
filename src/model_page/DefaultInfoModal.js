d741378ea45c0c42ea9877859dc0965c7c4a7aea
unknown <kevin018111@gmail.com>
2024-08-12 20:10:59 +0900

    update:인스타 띄우는 부분 수정중

--- src/model_page/DefaultInfoModal.js
+++ src/model_page/DefaultInfoModal.js
@@ -5,30 +5,30 @@ import "./../css/model/model.css";
 
 let DefaultInfoModal = () => {
 
-    const boxSize = [0.1, 6, 4];
+    const boxSize = [0.1, 25, 20];
 
     const [ref] = useBox(() => ({
         args: boxSize,
-        position: [6, 3, -8],
+        position: [30, 13, -30],
     }));
 
     return (
-        <mesh ref={ref} visible={true}>
+        <mesh ref={ref} visible={false}>
             <boxGeometry args={boxSize}/>
             <meshStandardMaterial color="lightblue"/>
 
             <Html position={[0, 0, 0]} rotation={[0, -(Math.PI / 2), 0]} center transform={true}>
                 <div
                     style={{
-                        width: `${boxSize[1] * 27}px`,
-                        height: `${boxSize[2] * 60.5}px`,
+                        width: `${boxSize[1] * 32}px`,
+                        height: `${boxSize[2] * 50}px`,
                         boxSizing: 'border-box',
                         background: 'rgb(0, 0, 0, 0.5)',
                     }}
                     className={"model_modal_container"}
                 >
                     <div>
-                        <img src={`${process.env.PUBLIC_URL}/img/intagram_profile_white3.png`} loading="lazy"
+                        <img src={`${process.env.PUBLIC_URL}/img/intagram_profile_white.png`} loading="lazy"
                              className={"instagram_profile_img"} alt={"인스타 프로필 이미지"}/>
                     </div>
                 </div>
