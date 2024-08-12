5942448fbda81d12fdfaf8d4af963d992279fba2
unknown <kevin018111@gmail.com>
2024-08-12 17:32:16 +0900

    feat:저작권 텍스트 설정 부분에 추가 및 지도 위치 변경

--- src/model_page/Browser.js
+++ src/model_page/Browser.js
@@ -8,6 +8,8 @@ let SelectIcon = ({icon, setProgram}) => {
             return <MapComponent/>
         case "spotify":
             return <SpotifyComponent/>
+        case "setting":
+            return <SettingComponent/>
         default:
             //setProgram("");
             break;
@@ -31,13 +33,9 @@ let MapComponent = () => {
     return (
         <div className={"mac_youtube_cp"}>
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.085489228413!2d144.96305761589207!3d-37.81410757975144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ce6e0!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1616710276799!5m2!1sen!2sus"
-                title="google map"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                loading="lazy"
-            />
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6347.3782684297585!2d126.81710368569334!3d37.30250081311309!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b6fa438180397%3A0xfb13490db1b671bf!2z7JWI7IKw7Zi47JuQ7LSI65Ox7ZWZ6rWQ!5e0!3m2!1sko!2skr!4v1723451449375!5m2!1sko!2skr"
+                width="1800" height="4000" allowFullScreen="" loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
     )
 }
@@ -50,6 +48,93 @@ let SpotifyComponent = () => {
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 src="https://open.spotify.com/embed/track/5elW2CKSoqjYoJ32AGDxf1?utm_source=generator"
                 loading="lazy"></iframe>
+        </div>
+    )
+}
+
+let SettingComponent = () => {
+    return (
+        <div className={"mac_youtube_cp"}>
+            <p style={{
+                fontSize: "100px",
+                color: "black",
+                padding: "1%"
+            }} className={"overflow-y-hidden"}>
+                안녕하세요. 넨이의 포트폴리오 입니다.
+                <br/>
+                깃허브 페이지로 작성되었으며 R3F를 사용한 모델링 기반 React 페이지 입니다.
+                <br/>
+
+                <p style={{
+                    fontSize: "70px"
+                }}>
+                    저작권
+                    <br/>
+                    모든 모델링은 무료 모델링 또는 직접 제작한 모델링이며 아래는 해당 무료 모델링에 대한 라이센스 txt입니다.
+                    <br/>
+
+                    1.Model Information:
+                    * title: Razer Setup Scene
+                    * source: https://sketchfab.com/3d-models/razer-setup-scene-00d1918300634af48489467002fcb308
+                    * author: brendan wood (https://sketchfab.com/brendanwood872)
+
+                    Model License:
+                    * license type: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+                    * requirements: Author must be credited. Commercial use is allowed.
+
+                    If you use this 3D model in your project be sure to copy paste this credit wherever you share it:
+                    This work is based on "Razer Setup Scene"
+                    (https://sketchfab.com/3d-models/razer-setup-scene-00d1918300634af48489467002fcb308) by brendan wood
+                    (https://sketchfab.com/brendanwood872) licensed under CC-BY-4.0
+                    (http://creativecommons.org/licenses/by/4.0/)
+                    2.IMPORTANT NOTICE: This license only applies if you downloaded this content as
+                    an unsubscribed user. If you are a premium user (ie, you pay a subscription)
+                    you are bound to the license terms described in the accompanying file
+                    "License premium.txt".
+
+                    ---------------------
+
+                    You must attribute the image to its author:
+
+                    In order to use a content or a part of it, you must attribute it to Freepik,
+                    so we will be able to continue creating new graphic resources every day.
+
+
+                    How to attribute it?
+
+                    For websites:
+
+                    Please, copy this code on your website to accredit the author:
+                    <a href="http://www.freepik.com">Designed by Freepik</a>
+
+                    For printing:
+
+                    Paste this text on the final work so the authorship is known.
+                    - For example, in the acknowledgements chapter of a book:
+                    "Designed by Freepik"
+
+
+                    You are free to use this image:
+
+                    - For both personal and commercial projects and to modify it.
+                    - In a website or presentation template or application or as part of your design.
+
+                    You are not allowed to:
+
+                    - Sub-license, resell or rent it.
+                    - Include it in any online or offline archive or database.
+
+                    The full terms of the license are described in section 7 of the Freepik
+                    terms of use, available online in the following link:
+
+                    http://www.freepik.com/terms_of_use
+
+                    The terms described in the above link have precedence over the terms described
+                    in the present document. In case of disagreement, the Freepik Terms of Use
+                    will prevail.
+
+                </p>
+            </p>
         </div>
     )
 }
