5c106fed2e988bce40a6c28faed1535c8ea3c235
unknown <kevin018111@gmail.com>
2024-08-16 17:28:51 +0900

    feat: modal component part 나누기 했음

--- src/model_page/modalComponent/Skill.js
+++ src/model_page/modalComponent/Skill.js
@@ -1,7 +1,15 @@
 let Skill = () => {
     return (
-        <div className={"modal_com_container"}>
+        <div className={"modal_com_container flex flex-wrap overflow-y-scroll scrollbar-hide"}>
+            <div className={"part_d lg:basis-1/3 md:basis-1/2 sm:basis-full"}>
 
+            </div>
+            <div className={"part_d lg:basis-1/3 md:basis-1/2 sm:basis-full"}>
+
+            </div>
+            <div className={"part_d lg:basis-1/3 md:basis-1/2 sm:basis-full"}>
+
+            </div>
         </div>
     )
 }
