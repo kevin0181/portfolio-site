15bcf99ecb4316e24a0c8acd133e9e78a3032bb3
kevin0181 <kevin018111@gmail.com>
2024-08-17 23:58:00 +0900

    feat: career 부분 추가중

--- src/model_page/modalComponent/Career.js
+++ src/model_page/modalComponent/Career.js
@@ -1,6 +1,22 @@
 let Career = () => {
     return (
-        <></>
+        <div
+            className={"modal_com_container flex flex-wrap overflow-y-scroll scrollbar-hide font-Kanit dark:text-gray-800"}>
+            <div className={"part_d_career part_d flex flex-col items-center"}>
+                <div className={"career_part"}>
+
+                </div>
+                <div className={"career_part"}>
+
+                </div>
+                <div className={"career_part"}>
+
+                </div>
+                <div className={"career_part"}>
+
+                </div>
+            </div>
+        </div>
     )
 }
 export default Career;
