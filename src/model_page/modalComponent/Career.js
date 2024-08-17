05ea075e9b1cee367331b2f775591f10ed63b018
kevin0181 <kevin018111@gmail.com>
2024-08-18 00:29:29 +0900

    update: career 부분 글 추가 해야됨'

--- src/model_page/modalComponent/Career.js
+++ src/model_page/modalComponent/Career.js
@@ -1,19 +1,47 @@
 let Career = () => {
     return (
         <div
-            className={"modal_com_container flex flex-wrap overflow-y-scroll scrollbar-hide font-Kanit dark:text-gray-800"}>
+            className={"modal_com_container flex flex-wrap overflow-y-scroll scrollbar-hide font-jua dark:text-gray-800"}>
             <div className={"part_d_career part_d flex flex-col items-center"}>
-                <div className={"career_part"}>
-
+                <div className={"career_part flex"}>
+                    <div className={"part_side_bar"}></div>
+                    <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
+                        <div className={"c_p_top"}>설계 가공</div>
+                        <div className={"c_p_body"}>00~00</div>
+                        <div className={"c_p_footer"}>공장에서 설계가공~</div>
+                    </div>
                 </div>
-                <div className={"career_part"}>
-
+                <div className={"career_part flex"}>
+                    <div className={"part_side_bar"}></div>
+                    <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
+                        <div className={"c_p_top"}>국비지원</div>
+                        <div className={"c_p_body"}>00~00</div>
+                        <div className={"c_p_footer"}>개발시작~</div>
+                    </div>
                 </div>
-                <div className={"career_part"}>
-
+                <div className={"career_part flex"}>
+                    <div className={"part_side_bar"}></div>
+                    <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
+                        <div className={"c_p_top"}>공군 병장 만기 전역</div>
+                        <div className={"c_p_body"}>00~00</div>
+                        <div className={"c_p_footer"}>전산병~</div>
+                    </div>
                 </div>
-                <div className={"career_part"}>
-
+                <div className={"career_part flex"}>
+                    <div className={"part_side_bar"}></div>
+                    <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
+                        <div className={"c_p_top"}>(주) 엘리스</div>
+                        <div className={"c_p_body"}>00~00</div>
+                        <div className={"c_p_footer"}>서울에서 진행하는 ~</div>
+                    </div>
+                </div>
+                <div className={"career_part flex"}>
+                    <div className={"part_side_bar"}></div>
+                    <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
+                        <div className={"c_p_top"}>대학교</div>
+                        <div className={"c_p_body"}>00~현재</div>
+                        <div className={"c_p_footer"}>재학중~</div>
+                    </div>
                 </div>
             </div>
         </div>
