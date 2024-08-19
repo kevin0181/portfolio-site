d5369e61681f140165ffb4bea69361412ba171b3
youngbeen you <kevin018111@gmail.com>
2024-08-19 17:37:08 +0900

    1

--- src/model_page/modalComponent/Resume.js
+++ src/model_page/modalComponent/Resume.js
@@ -1,6 +1,18 @@
 let Resume = () => {
     return (
-        <></>
+        <div
+            className={"modal_com_container flex flex-wrap overflow-y-scroll scrollbar-hide font-jua dark:text-gray-800"}>
+            <div className={"part_d_career part_d flex flex-col items-center"}>
+                <a href={"/portfolio-site/resume"} className={"text-blue-600 underline"}>크게 보기</a>
+                <iframe
+                    src="/portfolio-site/resume"
+                    title="resume Page"
+                    width="100%"
+                    height="auto"
+                    style={{border: 'none'}}
+                />
+            </div>
+        </div>
     )
 }
 
--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -1,6 +1,10 @@
-let Resume = ()=>{
-    return(
-        <></>
+import "./../css/resume.css";
+
+let Resume = () => {
+    return (
+        <div className={"resume_container"}>
+            이력서
+        </div>
     )
 }
 
