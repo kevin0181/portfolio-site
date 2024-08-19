ebbef8146bccdcfa8de28bc03a338bafd7e8fbc0
youngbeen you <kevin018111@gmail.com>
2024-08-19 13:34:46 +0900

    feat:이력서 작성 완료

--- src/model_page/modalComponent/Career.js
+++ src/model_page/modalComponent/Career.js
@@ -6,41 +6,50 @@ let Career = () => {
                 <div className={"career_part flex"}>
                     <div className={"part_side_bar"}></div>
                     <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
-                        <div className={"c_p_top"}>설계 가공</div>
-                        <div className={"c_p_body"}>00~00</div>
-                        <div className={"c_p_footer"}>공장에서 설계가공~</div>
+                        <div className={"c_p_top"}>(주) 네스텍코리아</div>
+                        <div className={"c_p_body"}>18.11~19.8</div>
+                        <div className={"c_p_footer"}>2D 캐드를 사용하여 도면 설계를 하였고, 솔리드 웍스를 통해 3D 도면 설계를 배웠습니다. 또한 설계 도면을 통해
+                            MCT 가공을 했습니다.
+                        </div>
                     </div>
                 </div>
                 <div className={"career_part flex"}>
                     <div className={"part_side_bar"}></div>
                     <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
-                        <div className={"c_p_top"}>국비지원</div>
-                        <div className={"c_p_body"}>00~00</div>
-                        <div className={"c_p_footer"}>개발시작~</div>
+                        <div className={"c_p_top"}>더 휴먼 아카데미 국비 지원 정보처리산업기사 과정 수료</div>
+                        <div className={"c_p_body"}>19.12~20.06</div>
+                        <div className={"c_p_footer"}>개발을 시작하게 된 계기이고, 개발에 대한 기본적인 틀을 잡게 되며 java, oracle, javascript,
+                            jsp를 배우게 되었습니다. 처음으로 jsp를 사용한 api 웹 프로젝트를 만들었습니다.
+                        </div>
                     </div>
                 </div>
                 <div className={"career_part flex"}>
                     <div className={"part_side_bar"}></div>
                     <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
                         <div className={"c_p_top"}>공군 병장 만기 전역</div>
-                        <div className={"c_p_body"}>00~00</div>
-                        <div className={"c_p_footer"}>전산병~</div>
+                        <div className={"c_p_body"}>20.12~22.09</div>
+                        <div className={"c_p_footer"}>정보체계병(전산병)으로 컴퓨터와 서버 및 정보 보안에 대한 다양한 지식을 배우게 되었습니다. 또한 공군의 장점을 살려
+                            다양한 프로젝트를 만들어 보았고, 해커톤 등 다양한 프로그램에 참가했습니다.
+                        </div>
                     </div>
                 </div>
                 <div className={"career_part flex"}>
                     <div className={"part_side_bar"}></div>
                     <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
                         <div className={"c_p_top"}>(주) 엘리스</div>
-                        <div className={"c_p_body"}>00~00</div>
-                        <div className={"c_p_footer"}>서울에서 진행하는 ~</div>
+                        <div className={"c_p_body"}>22.06~현재</div>
+                        <div className={"c_p_footer"}>서울 특별시에서 진행하는 인공지능 웹 개발 강사, 웹 개발 프로젝트 멘토, 기업 은행 신입 사원 멘토링 등 프리랜서
+                            강사로 일하게 되었습니다.
+                        </div>
                     </div>
                 </div>
                 <div className={"career_part flex"}>
                     <div className={"part_side_bar"}></div>
                     <div className={"career_part_body flex flex-col justify-center text-gray-800"}>
-                        <div className={"c_p_top"}>대학교</div>
-                        <div className={"c_p_body"}>00~현재</div>
-                        <div className={"c_p_footer"}>재학중~</div>
+                        <div className={"c_p_top"}>한국공학대학교</div>
+                        <div className={"c_p_body"}>23.03~현재</div>
+                        <div className={"c_p_footer"}>게임공학과에서 재학중이며, 게임 개발에 대한 꿈을 위해 C언어와 네트워크 등 다양한 기술을 공부하고 있습니다.
+                        </div>
                     </div>
                 </div>
             </div>
