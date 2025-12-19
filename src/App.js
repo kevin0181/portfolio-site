1271687179a1d93b5a37a32f9ff3577cf3773327
neneee0181 <2022180024@tukorea.ac.kr>
2025-12-19 19:14:17 +0900

    1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Razer from "./model_page/Razer";
 
 import "./css/app.css";
 import Doom from "./model_page/Doom";
+import Demo from "./demo/Demo";
 
 function App() {
     return (
@@ -20,6 +21,7 @@ function App() {
                     <Route path={"/resume"} element={<Resume/>}/>
                     <Route path={"/project"}/>
                     <Route path={"/doom"} element={<Doom/>}/>
+                    <Route path={"/demo"} element={<Demo/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
